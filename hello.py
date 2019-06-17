6e8be662be9dc171ad52433676bd2cdea6ccdad7
Thu Tran <thuchimney292@gmail.com>
2019-06-17 19:16:23 +0700

    glg

---
+++ hello.py
@@ -0,0 +1,15 @@
+from turtle import *
+speed(100)
+color("#548253")
+for v in range(100):
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    forward(100)
+    left(90)
+    left(2)
+
+mainloop()
